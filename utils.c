4178d4bcd98d1f8e3df7a371c0a29dfb066cd6c8
Denis Coutinho <56079372+DenisCoutinho@users.noreply.github.com>
2023-01-17 09:34:31 -0300

    Add files via upload

--- utils.c
+++ utils.c
@@ -2,60 +2,96 @@
 
 #define TAM_TAB 100
 
-enum {INT, LOG};
+enum
+{
+    INT,
+    LOG
+};
 
-
-struct elemTabSimbolos {
+struct elemTabSimbolos
+{
     char id[100];
     int end;
     int tip;
 } tabSimb[TAM_TAB], elemTab;
 
-int posTab = 0; 
+int posTab = 0;
 
-void maiscula (char *s) {
-    for(int i = 0; s[i]; i++)
+void maiscula(char *s)
+{
+    for (int i = 0; s[i]; i++)
         s[i] = toupper(s[i]);
 }
 
 int buscaSimbolo(char *id)
 {
     int i;
-    //maiscula(id);
+    // maiscula(id);
     for (i = posTab - 1; strcmp(tabSimb[i].id, id) && i >= 0; i--)
         ;
-    if (i == -1) {
+    if (i == -1)
+    {
         char msg[200];
         sprintf(msg, "Identificador [%s] não encontrado!", id);
         yyerror(msg);
     }
     return i;
 }
-void insereSimbolo (struct elemTabSimbolos elem) {
-    int i; 
-    //maiscula(elem.id);
+void insereSimbolo(struct elemTabSimbolos elem)
+{
+    int i;
+    // maiscula(elem.id);
     if (posTab == TAM_TAB)
         yyerror("Tabela de Simbolos Cheia!");
     for (i = posTab - 1; strcmp(tabSimb[i].id, elem.id) && i >= 0; i--)
         ;
-    if (i != -1) {
+    if (i != -1)
+    {
         char msg[200];
         sprintf(msg, "Identificador [%s] duplicado!", elem.id);
         yyerror(msg);
     }
-    tabSimb[posTab++] = elem; 
+    tabSimb[posTab++] = elem;
 }
 
-void mostraTabela () {
+void mostraTabela()
+{
     puts("Tabela de Simbolos");
     puts("------------------");
     printf("\n%30s | %s | %s \n", "ID", "END", "TIP");
-    for(int i = 0; i < 50; i++) 
+    for (int i = 0; i < 50; i++)
         printf(".");
-    for(int i = 0; i < posTab; i++)
+    for (int i = 0; i < posTab; i++)
         printf("\n%30s | %3d | %s\n", tabSimb[i].id, tabSimb[i].end, tabSimb[i].tip == INT ? "INT" : "LOG");
     printf("\n");
 }
+
+/* void testaAritimetico()
+{
+    int t1 = desempilha();
+    int t2 = desempilha();
+    if (t1 != INT || t2 != INT)
+        yyerror("Incompatibilidade de tipo");
+    empilha(INT);
+} */
+
+/* void testaRelaciona()
+{
+    int t1 = desempilha();
+    int t2 = desempilha();
+    if (t1 != INT || t2 != INT)
+        yyerror("Incompatibilidade de tipo");
+    empilha(LOG);
+} */
+
+/* void testaLogico()
+{
+    int t1 = desempilha();
+    int t2 = desempilha();
+    if (t1 != LOG || t2 != LOG)
+        yyerror("Incompatibilidade de tipo");
+    empilha(LOG);
+} */
 
 // estrutura da pilha semantica
 // usada para enderecos, variaveis, rotulos
@@ -64,14 +100,25 @@ void mostraTabela () {
 int pilha[TAM_PIL];
 int topo = -1;
 
-void empilha (int valor) {
+void empilha(int valor)
+{
     if (topo == TAM_PIL)
         yyerror("Pilha semântica cheia");
     pilha[++topo] = valor;
 }
 
-int desempilha() {
+int desempilha()
+{
     if (topo == -1)
         yyerror("Pilha semântica vazia");
     return pilha[topo--];
+}
+
+void testaTipo(int tipo1, int tipo2, int ret)
+{
+    int t1 = desempilha();
+    int t2 = desempilha();
+    if (t1 != tipo1 || t2 != tipo2)
+        yyerror("Incompatibilidade de tipo");
+    empilha(ret);
 }
